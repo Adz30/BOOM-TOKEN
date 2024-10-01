22b6921e18df9e87188fd24d3420a300b47d5a2a
adam <kaw94@hotmail.co.uk>
2024-10-01 19:53:18 +0100

    6.token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,18 +14,25 @@ contract Token{
 
 	
 	mapping(address => uint256)public balanceOf;
+	mapping(address => mapping(address => uint256))public allowance;
 
 	event Transfer(
 		address indexed from,
 		address indexed to,
 		uint256 value 
 		);
+
+	event Approval(
+		address indexed owner,
+		address indexed spender,
+		uint256 value
+		);
 	
 	constructor(
 		string memory _name, 
 		string memory _symbol, 
 		uint256 _totalSupply
-	) {
+	){
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
@@ -52,6 +59,19 @@ contract Token{
 		emit Transfer(msg.sender, _to, _value);
 
 		return true;
+
+	}
+	function approve(address _spender, uint256 _value) 
+		public 
+		returns (bool success)
+	{
+		require(_spender != address(0));
+		
+		allowance[msg.sender][_spender] = _value;
+		
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+
 
 	}
 }

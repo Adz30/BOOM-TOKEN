346d36ce024036e354785b91127bf693606cc291
adam <kaw94@hotmail.co.uk>
2024-10-03 19:46:36 +0100

    8. token deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,8 @@
   //fetch contract to deploy
   const Token = await ethers.getContractFactory("Token")
   //deploy contract
-  const token = await Token.deploy()
+  const token = await Token.deploy('Boom token', 'BMTK', '10000000')
+  gasPrice: ethers.utils.parseUnits('5', 'gwei'),
   await token.deployed()
   console.log(`Token Deployed to: ${token.address}`)
 }
@@ -10,6 +11,6 @@
   main() 
   .then(() => process.exit(0))
   .catch((error) => {
-  console.error("this didnt work", Error);
+  console.error("this didnt work", error);
   process.exit(1);
 });
